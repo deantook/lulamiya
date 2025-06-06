881e7957a60e21bb12dc785fbcd01f2d748c7451
deantook <chenyunlong99@outlook.com>
2025-06-06 13:16:56 +0800

    init

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package lulamiya
 
 // Greet 返回一个友好的问候
 func Greet(name string) string {
-	return "Hello, " + name + "! Welcome to lulamiya!"
+	return "Hello, " + name + "! Welcome to lulamiya!!"
 }
 
 // Version 返回当前模块版本
