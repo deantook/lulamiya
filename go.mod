e9396e7336da14e122fa13e23a16cd30316fc6d1
deantook <chenyunlong99@outlook.com>
2025-06-06 11:55:52 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/deantook/lulamiya
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package lulamiya
+
+// Greet 返回一个友好的问候
+func Greet(name string) string {
+	return "Hello, " + name + "! Welcome to lulamiya!"
+}
+
+// Version 返回当前模块版本
+func Version() string {
+	return "v1.0.0"
+}
